46b22a014c6398cfc382f5cd2a63ec50030912ae
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-08-26 20:05:05 +0100

    modify the logic'

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,22 @@
 import express from 'express';
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv';
+import employeeRoutes from './routes/employeeRoutes.js';
+import adminRoutes from './routes/adminRoutes.js';
+import attendanceRoutes from './routes/attendanceRoutes.js';
 
-import employeeRoutes from './routes/employeeRoutes';
-import adminRoutes from './routes/adminRoutes';
-import attendanceRoutes from './routes/attendanceRoutes';
+dotenv.config();
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
 
-app.use('/employess', employeeRoutes(prisma))
-app.use('/attendances', attendanceRoutes(prisma))
-app.use('/admins', adminRoutes(prisma))
+app.use('/employees', employeeRoutes(prisma));
+app.use('/attendances', attendanceRoutes(prisma));
+app.use('/admins', adminRoutes(prisma));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
 });
