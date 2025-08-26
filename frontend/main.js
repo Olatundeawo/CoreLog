5dc0804c2e3c5bf0addc7524e525a3ae89947d69
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-08-26 21:10:57 +0100

    modify the logic

--- frontend/main.js
+++ frontend/main.js
@@ -6,10 +6,21 @@ function createWindow() {
     height: 400,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
     },
   });
   win.loadFile("index.html");
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
 
