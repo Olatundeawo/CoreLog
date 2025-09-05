a67002cfdfb9a93cd4184cb6e8b6514e887ed0b7
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-09-05 17:36:00 +0100

    routes that verify and create employee

---
+++ admin-dashboard/src/components/employeeCreate.jsx
@@ -0,0 +1,11 @@
+import React, { useRef, useState } from "react";
+
+function EmployeeCreate() {
+  return (
+    <div>
+      <h1>This the employee registration table.</h1>
+    </div>
+  );
+}
+
+export default EmployeeCreate;
