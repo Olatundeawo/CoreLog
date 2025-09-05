0d1a74f8d69103120e8caa5adc34b407d4147be9
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-09-05 17:27:28 +0100

    modifty

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import AdminCreate from "./components/adminCreate";
+import EmployeeCreate from "./components/employeeCreate";
+import FaceEmbedder from "./components/faceEmbedder";
 
 function App() {
   return (
     <div>
       <AdminCreate />
+      {/* <EmployeeCreate /> */}
+      <FaceEmbedder />
     </div>
   );
 }
