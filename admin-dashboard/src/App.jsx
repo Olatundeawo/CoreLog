ef238112eada2942090c5d2a779b45d7556f5163
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-09-07 14:43:34 +0100

    remove the faceEmbeding from app jsx

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import AdminCreate from "./components/adminCreate";
 import EmployeeCreate from "./components/employeeCreate";
-import FaceEmbedder from "./components/faceEmbedder";
 
 function App() {
   return (
     <div>
       <AdminCreate />
-      {/* <EmployeeCreate /> */}
-      <FaceEmbedder />
+      <EmployeeCreate />
     </div>
   );
 }
